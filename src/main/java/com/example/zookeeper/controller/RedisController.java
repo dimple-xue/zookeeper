39706f4916ae51777288eb59791e73b972ee64e4
薛朋朋 <lynn_xue@gomro.cn>
2021-09-24 15:28:44 +0800

    Initial commit

---
+++ src/main/java/com/example/zookeeper/controller/RedisController.java
@@ -0,0 +1,17 @@
+package com.example.zookeeper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author xue
+ * @date 2021/4/28
+ */
+@Controller
+@RequestMapping("redis")
+public class RedisController {
+  public void aa(){
+
+  }
+
+}
